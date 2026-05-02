14a4fcf4722d2c810b438dce4159ebf3ca23396d
Gabriel <gabdasilva96@gmail.com>
2026-05-02 16:39:41 +0100

    Add basic test

--- Csharp/GildedRoseTests/GildedRoseTests.cs
+++ Csharp/GildedRoseTests/GildedRoseTests.cs
@@ -7,11 +7,44 @@ namespace GildedRoseTests;
 public class GildedRoseTests
 {
     [Fact]
-    public void UpdateQuality_DoesNotChangeItemName()
+    public void UpdateQuality_DoesNotChangeNormalItemName()
     {
-        IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
+        // Arrange
+        IList<Item> Items = new List<Item> { new() { Name = "+5 Dexterity Vest", SellIn = 0, Quality = 0 } };
         GildedRose app = new GildedRose(Items);
+
+        // Act
         app.UpdateQuality();
-        Assert.Equal("foo", Items[0].Name);
+
+        // Assert
+        Assert.Equal("+5 Dexterity Vest", Items[0].Name);
+    }
+
+    [Fact]
+    public void UpdateQuality_ReducesNormalItemSellInByOne()
+    {
+        // Arrange
+        IList<Item> Items = new List<Item> { new() { Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20 } };
+        GildedRose app = new GildedRose(Items);
+
+        // Act
+        app.UpdateQuality();
+
+        // Assert
+        Assert.Equal(9, Items[0].SellIn);
+    }
+
+    [Fact]
+    public void UpdateQuality_ReducesNormalItemQualityByOneBeforeSellDate()
+    {
+        // Arrange
+        IList<Item> Items = new List<Item> { new() { Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20 } };
+        GildedRose app = new GildedRose(Items);
+
+        // Act
+        app.UpdateQuality();
+
+        // Assert
+        Assert.Equal(19, Items[0].Quality);
     }
 }
