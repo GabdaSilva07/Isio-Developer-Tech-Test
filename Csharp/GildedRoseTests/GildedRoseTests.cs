422d0e8dcb5bce38ad721fea686f68cd15c0bd78
Gabriel <gabdasilva96@gmail.com>
2026-05-03 18:59:57 +0100

    Added backstage pass threshold tests

--- Csharp/GildedRoseTests/GildedRoseTests.cs
+++ Csharp/GildedRoseTests/GildedRoseTests.cs
@@ -105,6 +105,34 @@ public class GildedRoseTests
     }
 
     [Fact]
+    public void UpdateQuality_IncreasesBackstagePassQualityByThreeWhenSevenDaysOrLess()
+    {
+        // Arrange
+        IList<Item> Items = new List<Item> { new() { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 7, Quality = 20 } };
+        GildedRose app = new GildedRose(Items);
+
+        // Act
+        app.UpdateQuality();
+
+        // Assert
+        Assert.Equal(23, Items[0].Quality);
+    }
+
+    [Fact]
+    public void UpdateQuality_IncreasesBackstagePassQualityByFourWhenTwoDaysOrLess()
+    {
+        // Arrange
+        IList<Item> Items = new List<Item> { new() { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 2, Quality = 20 } };
+        GildedRose app = new GildedRose(Items);
+
+        // Act
+        app.UpdateQuality();
+
+        // Assert
+        Assert.Equal(24, Items[0].Quality);
+    }
+
+    [Fact]
     public void UpdateQuality_ReducesConjuredItemQualityByTwoBeforeSellDate()
     {
         // Arrange
